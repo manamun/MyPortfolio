ba63d0552bd6743a63a83acc38ac06eebfdc9a61
manamun <satomana0513@gmail.com>
2024-10-29 04:21:03 +0900

    public画像の試行錯誤

--- portfolio-cite/app/components/Works/Works.js
+++ portfolio-cite/app/components/Works/Works.js
@@ -6,21 +6,20 @@ export default function Works() {
 
     const works =[
         {
-          image: '/images/BachelorSystem.jpg',
+          image: './images/BachelorSystem.jpg',
           title: '運動軌道計測システム',
           description: '学部時代の卒業論文で作成したシステムです．3DSystems社の"Phantom Premium 1.5HF"を使用しており，到達運動時の手先の運動軌道・力のプロファイルを取得できます．',
           githubLink: 'https://github.com/manamun/Thesis-experiment-move-condition-',
         },
         {
-          image: '/images/WebCite.jpg',
+          image: './images/WebCite.jpg',
           title: 'My Portfolio Cite',
-          description: 'このサイト自体です．自分自身のスキルや作品について知ってもらうためのサイトです．これからもアップデートしていく予定です．',
+          description: 'このサイト自体です．自分自身のスキルや作品について知ってもらうためのサイトです．Github Pagesの仕様で静的ですが，これからもアップデートしていく予定です．',
           githubLink: 'https://github.com/manamun/MyPortfolio',
         },
       ];
 
       return (
-        <FadeIn>
             <Box
             sx={{
                 display: "flex",
@@ -46,6 +45,5 @@ export default function Works() {
                     </Grid2>
                 </Box>
             </Box>
-        </FadeIn>
       );
 }
